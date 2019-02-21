9b77237ebe719ea6f8eeca551aede4f5ffc8de30
bujxu <30498959+bujxu@users.noreply.github.com>
2019-02-21 20:32:36 +0800

    x

--- application/database.php
+++ application/database.php
@@ -17,9 +17,9 @@ return [
     // 数据库名
     'database'        => 'mtjs',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'admin',
     // 密码
-    'password'        => '',
+    'password'        => '!share123',
     // 端口
     'hostport'        => '',
     // 连接dsn
